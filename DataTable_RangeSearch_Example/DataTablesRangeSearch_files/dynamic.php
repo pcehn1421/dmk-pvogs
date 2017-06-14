a017def0bcc64965103649baf7ba869f08bdfa6f
Jerry <jerrychen9657@gmail.com>
2017-06-14 11:57:56 -0500

    range search working

---
+++ DataTable_RangeSearch_Example/DataTablesRangeSearch_files/dynamic.php
@@ -0,0 +1,8 @@
+window._site.forumUser = "";
+$(document).ready( function () {
+	window._site.comments( $('div.content div.comments'), [] );
+} );window._site.page = "examples\/plug-ins\/range_filtering.html";
+
+$(document).ready( function () {
+	window._site.dynamicLoaded();
+} );
