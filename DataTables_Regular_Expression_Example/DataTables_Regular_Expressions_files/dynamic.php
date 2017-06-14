1be20e8efaeaa8f76684d3adcb2027e2b5b72e68
Jerry <jerrychen9657@gmail.com>
2017-06-14 16:02:35 -0500

    Integerated && Searching complete

---
+++ DataTables_Regular_Expression_Example/DataTables_Regular_Expressions_files/dynamic.php
@@ -0,0 +1,8 @@
+window._site.forumUser = "";
+$(document).ready( function () {
+	window._site.comments( $('div.content div.comments'), [] );
+} );window._site.page = "examples\/api\/regex.html";
+
+$(document).ready( function () {
+	window._site.dynamicLoaded();
+} );
