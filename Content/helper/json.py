da54cab15b31afa0ede968d865d097021b898d30
Jerry <jerrychen9657@gmail.com>
2017-06-23 12:52:25 -0500

    ajax added

--- Content/helper/json.py
+++ Content/helper/json.py
@@ -1,5 +1,5 @@
 sourceFile = "./sample.txt"
-targetFile = "./targetSample.txt"
+targetFile = "./JSONSample.txt"
 
 t = open(targetFile, 'w').close()
 t = open(targetFile, 'w')
@@ -10,15 +10,26 @@ with open(sourceFile, 'r') as file:
     for line in file:
         t.write("\t\t[\n")
         counter = 0
+        proteins = 0
         for word in line.split('\t'):
+            if counter == 1 or counter == 7 or counter == 14:
+                counter = counter +  1
+                continue
+            if counter == 4:
+                proteins = int(word)
+                counter = counter + 1
+                continue
             t.write('\t\t\t"')
-            t.write(word[:-1])
-            t.write(' "')
-            counter = counter + 1
-            if counter != 15:
-                t.write(",\n")
+            if counter == 3:
+                t.write(word[3:] + "\",\n")
+            elif counter == 5:
+                proteins = proteins + int(word)
+                t.write(str(proteins) + "\",\n")
+            elif counter != 13:
+                t.write(word + "\",\n")
             else:
-                t.write("\n")
+                t.write(word + "\"\n")
+            counter = counter + 1
         t.write("\t\t],\n")
 
 t.write("\t]\n}")
---
+++ helper/json.py
@@ -0,0 +1,24 @@
+sourceFile = "./sample.txt"
+targetFile = "./targetSample.txt"
+
+t = open(targetFile, 'w').close()
+t = open(targetFile, 'w')
+
+t.write('{\n\t"data" : [\n')
+
+with open(sourceFile, 'r') as file:
+    for line in file:
+        t.write("\t\t[\n")
+        counter = 0
+        for word in line.split('\t'):
+            t.write('\t\t\t"')
+            t.write(word[:-1])
+            t.write('"')
+            counter = counter + 1
+            if counter != 15:
+                t.write(",\n")
+            else:
+                t.write("\n")
+        t.write("\t\t],\n")
+
+t.write("\t]\n}")
