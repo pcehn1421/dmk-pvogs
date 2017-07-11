a40fc6293a5b05071d47e4697a13e15715d013bb
Jerry <jerrychen9657@gmail.com>
2017-07-10 21:28:29 -0500

    all in one rowfish

--- Content/helper/myjs.js
+++ Content/helper/myjs.js
@@ -26,9 +26,16 @@
       $('#rangeSearch').toggle('slow');
     });
 
+  $('<div class="loading">Loading</div>').appendTo('body');
+
     var table = $('#example').DataTable({
-      "order": [[1, 'dsc']],
-      "lengthMenu": [[10, 25, 50, -1],[10, 25, 50, "All"]],
+      "order": [
+        [1, 'dsc']
+      ],
+      "lengthMenu": [
+        [10, 25, 50, -1],
+        [10, 25, 50, "All"]
+      ],
       "select": true,
       "dom": 'Blfrtip',
       "buttons": [
@@ -36,8 +43,7 @@
         {
           extend: 'collection',
           text: 'Export options',
-          buttons: [
-            {
+          buttons: [{
               extend: 'pdf',
               text: 'pdf all'
             },
@@ -106,74 +112,69 @@
           ]
         }
       ],
-      "ajax": "./helper/sample/target.txt",
+      "ajax": "./helper/sample/JSONSample.txt",
 
 
       initComplete: function() {
+        $('div.loading').remove();
         this.api().columns([6, 7, 8, 9, 10]).every(function() {
-          var column = this;
+            var column = this;
+            var selectFooter =
+              $('<select><option value="">Show All</option><select>')
+              .appendTo($(column.footer()).empty())
+              .on('change', function() {
+                var val = $.fn.dataTable.util.escapeRegex(
+                  $(this).val()
+                );
 
-          var selectFooter = $('<select><option value=""></option></select>')
-            .appendTo($(column.footer()).empty())
-            .on('change', function() {
-              var val = $.fn.dataTable.util.escapeRegex(
-                $(this).val()
-              );
+                column
+                  .search(val ? '^' + val + '$' : '', true, true, false)
+                  .draw();
+              });
 
-              column
-                .search(val ? '^' + val + '$' : '', true, false)
-                .draw();
+            column.data().unique().sort().each(function(d, j) {
+              selectFooter.append('<option value="' + d + '">' + d + '</option>');
+
             });
-
-          column.data().unique().sort().each(function(d, j) {
-            selectFooter.append('<option value="' + d + '">' + d + '</option>');
           });
 
-        });
-
-
-      }
-    });
-
-var counter = 0;	
-    // generating header search boxes
-    $('#example .filter th').each(function() {
-      var title = $(this).text();
-      if (counter < 6) {
-      $(this).html('<input type="text" placeholder="Search ' + title + '" />');
-    	}	
-      counter = counter + 1;
-    });
-
-    table.columns().every(function() {
-      var that = this;
-
-      $('input', this.footer() ).on('keyup change', function() {
-        if( that.search() !== this.value) {
-          that.search(this.value)
+        this.api().columns([0, 1, 2, 3, 4, 5]).every(function() {
+            var column = this;
+            //first add the boxes
+            //then apply the actual search
+            var searchFooter =
+              $('<input type="text" placeholder="search"/>')
+              .appendTo($(column.footer()))
+              .on('keyup', function() {
+                var val = $.fn.dataTable.util.escapeRegex(
+                  $(this).val()
+                );
+            column
+              .search(val,false,true,true)
               .draw();
+              });
+          });
         }
-      });
     });
 
     $('#example tbody').on('click', 'tr', function() {
       $(this).toggleClass('selected');
     });
 
-    $('button').click(function(){
-      alert(table.rows('.selected').data().length+'row(s) selected');
+    $('button').click(function() {
+      alert(table.rows('.selected').data().length + 'row(s) selected');
     });
 
-        $('#example tbody').on('click', 'tr', function() {
-          if ($(this).hasClass('selected')) {
-            $(this).removeClass('selected');
-          } else {
-            table.$('tr.selected').removeClass('selected');
-            $(this).addClass('selected');
-          }
-        });
+    $('#example tbody').on('click', 'tr', function() {
+      if ($(this).hasClass('selected')) {
+        $(this).removeClass('selected');
+      } else {
+        table.$('tr.selected').removeClass('selected');
+        $(this).addClass('selected');
+      }
+    });
 
-        $('button').click(function() {
-          table.row('.selected').remove().draw(false);
-        });
+    $('button').click(function() {
+      table.row('.selected').remove().draw(false);
+    });
   });
